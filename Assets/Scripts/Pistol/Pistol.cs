58b4632e537d5a4ba4503decbf5bf26d2850702a
idontknowwhatusername <userilovelikee@gmail.com>
2023-07-22 15:19:43 +0300

    почти доделал пистолет

--- Assets/Scripts/Pistol/Pistol.cs
+++ Assets/Scripts/Pistol/Pistol.cs
@@ -3,34 +3,47 @@ using UnityEngine;
 public class Pistol : MonoBehaviour
 {
     [SerializeField] private KeyCode _shootKey;
-    [SerializeField] private Bullet _bulletPrefab;
     [SerializeField] private float _colldown;
-    private float _time = 0f;
+    [SerializeField] private float _maxDistance;
+    [SerializeField] private Camera _camera;
+    private float _time;
     private Transform _bulletSpawnPoint;
     private ParticleSystem _particle;
-
+    private AudioSource _audio;
 
     private void Start()
     {
+
         GameObject bulletSpawnPoint = GameObject.FindWithTag("BulletSpawnPoint");
+        _audio = GetComponent<AudioSource>();
         _particle = bulletSpawnPoint.GetComponent<ParticleSystem>();
         _bulletSpawnPoint = bulletSpawnPoint.transform;
+        _time = _colldown;
     }
 
     private void Update()
     {
-        _time += Time.deltaTime;
-        if (Input.GetKeyDown(_shootKey) && _time >= _colldown)
+        _time -= Time.deltaTime;
+        if (Input.GetKeyDown(_shootKey) && _time <= 0)
         {
-            _time = 0f;
-            _particle.Play();
-            Bullet bullet = Instantiate(
-                _bulletPrefab.gameObject,
-                _bulletSpawnPoint.position,
-                Quaternion.identity
-            ).GetComponent<Bullet>();
-            Vector3 direction = _bulletSpawnPoint.forward;
-            bullet.Fly(direction);
+            Shoot();
         }
+    }
+
+    private void Shoot()
+    {
+        Vector3 origin = _camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+        _time = 0f;
+        _particle.Play();
+        _audio.Play();
+        if (Physics.Raycast(origin, _camera.transform.forward, out RaycastHit hit, _maxDistance))
+        {
+            HandleHit(hit);
+        }
+    }
+
+    private void HandleHit(RaycastHit hit)
+    {
+        print("Hit");
     }
 }
